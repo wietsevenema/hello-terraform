15a6ee0981da6d4028c055493d861b3554ce437b
Wietse Venema <wietsevenema@gmail.com>
2020-11-24 21:14:05 +0100

    Uncomment access policy

--- service-hello.tf
+++ service-hello.tf
@@ -1,27 +1,27 @@
 resource google_cloud_run_service "hello" {
-  name     = "hello"
+  name     = "hello-terraform"
   location = "us-central1"
 
   template {
     spec {
       containers {
-        image = "gcr.io/cloud-run-book/hello"
+        image = "gcr.io/cloud-run-book/inspect"
       }
     }
   }
 }
 
-data google_iam_policy "run_invoke_all_users" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
+# data google_iam_policy "run_invoke_all_users" {
+#   binding {
+#     role = "roles/run.invoker"
+#     members = [
+#       "allUsers",
+#     ]
+#   }
+# }
 
-resource google_cloud_run_service_iam_policy "hello_all_users" {
-  service     = google_cloud_run_service.hello.name
-  location    = google_cloud_run_service.hello.location
-  policy_data = data.google_iam_policy.run_invoke_all_users.policy_data
-}
+# resource google_cloud_run_service_iam_policy "hello_all_users" {
+#   service     = google_cloud_run_service.hello.name
+#   location    = google_cloud_run_service.hello.location
+#   policy_data = data.google_iam_policy.run_invoke_all_users.policy_data
+# }
